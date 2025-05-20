8d0d3f6483f3772f2ecb4d32af774d5e9eb268c0
dongyoon7212 <dongyoon7212@naver.com>
2025-05-20 21:49:45 +0900

    Feat: MultiArrayLoop

--- src/_14_Array/Array02.java
+++ src/_14_Array/Array02.java
@@ -6,13 +6,13 @@ public class Array02 {
 
         String[] countries = {"Hungary", "Croatia", "Slovenia", "Austria", "Czech"};
         //반복문 for문을 통해 배열 순회
-//        for (int i = 0; i < 5; i++) {
-//            System.out.println(countries[i]);
-//        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println(countries[i]);
+        }
 
-//        for (int i = 0; i < countries.length; i++) {
-//            System.out.println(countries[i]);
-//        }
+        for (int i = 0; i < countries.length; i++) {
+            System.out.println(countries[i]);
+        }
 
         //enhanced for (for-each) 향상된 for문
         for (String country : countries) {
