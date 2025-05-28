bf11deb501804de20e4f2b07cbd94052c50d5d86
dongyoon7212 <dongyoon7212@naver.com>
2025-05-28 19:50:02 +0900

    Feat: interface

---
+++ src/_25_casting/Control/CentralControl.java
@@ -0,0 +1,10 @@
+package _25_casting.Control;
+
+public class CentralControl {
+
+    private Power[] deviceArray;
+
+    public CentralControl(Power[] deviceArray) {
+        this.deviceArray = deviceArray;
+    }
+}
---
+++ src/_25_casting/Control/Main.java
@@ -0,0 +1,10 @@
+package _25_casting.Control;
+
+public class Main {
+    public static void main(String[] args) {
+        //Power 인터페이스
+        //on(), off()
+        //Tv, Computer, Speaker, LEDLight, Mouse, SmartPhone
+        //재정의하고 고유메소드 1개
+    }
+}
